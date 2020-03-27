463c10e8503da34d1e7c40cc7d7ec4f05726280f
George Bockari <bitwhys@gmail.com>
2020-03-26 20:44:35 -0700

    feat: add basic card design, missing background image

---
+++ stories/Markup/Loop-01/Step-03/Avatar.vue
@@ -0,0 +1,38 @@
+<template>
+  <!--  In a real life use case this outer tag would probably be a link -->
+  <span
+    class="avatar-root cursor-pointer border border-gray-400 p-2 rounded-full"
+  >
+    <svg
+      class="h-8"
+      fill="#000000"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm0 14.2c-2.5 0-4.71-1.28-6-3.22.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08-1.29 1.94-3.5 3.22-6 3.22z"
+      />
+    </svg>
+  </span>
+</template>
+
+<script>
+export default {
+  name: "Avatar",
+  props: {
+    color: {
+      type: String,
+    },
+  },
+}
+</script>
+
+<style scoped>
+.avatar-root {
+  @apply transition duration-150;
+  box-shadow: 0 0 0 1px transparent;
+}
+.avatar-root:hover {
+  box-shadow: 0 0 0 5px #ededed;
+}
+</style>
---
+++ stories/Markup/Loop-01/Step-03/Solution.vue
@@ -0,0 +1,53 @@
+<template>
+  <article class="bg-white rounded px-20 py-12">
+    <header class="mb-10">
+      <div class="mb-4 flex justify-between items-center">
+        <h2 class="text-2xl text-gray-900 font-semibold">SimpleWheels</h2>
+        <avatar />
+      </div>
+      <p class="font-semibold text-gray-500" style="font-size: .9rem">
+        Thanks for your order #2357654, your package will be on its way shortly.
+      </p>
+    </header>
+    <div class="orders flex flex-col justify-between">
+      <div v-for="(order, i) in orders" :key="i" class="flex justify-between">
+        <span class="font-semibold capitalize text-gray-600 text-lg">{{
+          order.name
+        }}</span
+        ><span class="text-gray-800 font-bold text-xl">${{ order.price }}</span>
+      </div>
+      <div class="flex justify-between">
+        <span class="font-bold uppercase tracking-wide text-gray-600 text-lg"
+          >total</span
+        ><span class="text-gray-800 font-bold text-xl">${{ total }}</span>
+      </div>
+    </div>
+  </article>
+</template>
+
+<script>
+import Avatar from "./Avatar"
+export default {
+  name: "Solution",
+  components: { Avatar },
+  props: {
+    orders: {
+      type: Array,
+      required: true,
+    },
+  },
+  computed: {
+    total() {
+      return this.orders.reduce((sum, currentOrder) => {
+        return (sum += currentOrder.price)
+      }, 0)
+    },
+  },
+}
+</script>
+
+<style scoped>
+.orders > * + * {
+  @apply mt-4;
+}
+</style>
